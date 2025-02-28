8ec4382216e8ee5478e164334f396aa784fb5c83
Manu Dev <manudev0004@gmail.com>
2025-02-28 17:26:21 +0530

    added animation card

---
+++ pariksha_setu/src/Components/Home/Animation.jsx
@@ -0,0 +1,20 @@
+import { AnimatedBeamDemo } from "@/components/easyui/animated-beam";
+
+const CustomIcons = {
+    image1: () => <Image src="https://www.svgrepo.com/show/303201/facebook-like-logo.svg" alt="1Password" width={24} height={24} />,
+    image2: () => <Image src="https://www.svgrepo.com/show/306500/openai.svg" alt="ActiveCampaign" width={30} height={30} />,
+    image3: () => <Image src="https://www.svgrepo.com/show/303145/instagram-2-1-logo.svg" alt="React Logo" width={24} height={24} />,
+    image4: () => <Image src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg" alt="Twitter" width={24} height={24} />,
+    image5: () => <Image src="https://www.svgrepo.com/show/303110/apple-black-logo.svg" alt="Netflix" width="24" height="24" />,
+    image6: () => <Image src="https://www.svgrepo.com/show/303147/whatsapp-icon-logo.svg" alt="Robinhood" width={34} height={34} />,
+    image7: () => <Image src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook" width={24} height={24} />,
+};
+
+export default function Home() {
+   <AnimatedBeamDemo
+        icons={CustomIcons}
+        beamColor="#ff4040"
+        beamSpeed={10}
+        className="custom-class"
+    />
+}
--- pariksha_setu/src/Pages/Home/HomePage.jsx
+++ pariksha_setu/src/Pages/Home/HomePage.jsx
@@ -5,11 +5,15 @@ import FeatureSection from "../../Components/Home/FeatureSection";
 import TestimonialSection from "../../Components/Home/TestimonialSection";
 import CTASection from "../../Components/Home/CTASection";
 import StatsSection from "../../Components/Home/StatsSection";
+import { AnimatedBeamDemo } from "../../Components/easyui/animated-beam";
 
 const HomePage = () => {
   return (
     <div className="pt-16">
       <HeroSection />
+
+      <AnimatedBeamDemo className="h-[650px] mt-8 bg-[rgba(247,247,247,0.8)]"/>
+
       <FeatureSection />
       <StatsSection />
       <TestimonialSection />
